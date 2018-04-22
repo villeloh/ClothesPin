bf298ff9f156967f8ae9a99c381ebf8bd5598584
Ville L <villelohkovuori1@gmail.com>
2018-04-22 19:41:26 +0300

    did a major upgrade on the image-downloading function. auto-creating of folders, adding items to existing db categories, retrieving potentially 2000 pics at once

--- ml-train/fetch-images/fetch.js
+++ ml-train/fetch-images/fetch.js
@@ -11,6 +11,7 @@
 
 const fetch = require("node-fetch");
 const download = require('image-downloader');
+const fse = require('fs-extra');
 
 const imageObjModule = require('./image.js');
 const ImageObj = imageObjModule.ImageObj;
@@ -19,8 +20,17 @@ const categoryObjModule = require('./category.js');
 const Category = categoryObjModule.CategoryObj;
 
 const BASE_API_URL = 'https://api.huuto.net/1.1/';
-const ABS_ROOT_IMG_FOLDER = 'C:/Users/VilleL/Desktop/ClothesPin/ml-train/fetch-images/images/'; // used when making the image objects. this needs to be changed for it to work on your computer (duh)
-const ROOT_DEST_FOLDER = '/Users/VilleL/Desktop/ClothesPin/ml-train/fetch-images/images/'; // used when dl'ding the images... it hides the 'C:' for some reason. -.- again, change this for yourself
+
+// used when making the image objects. this needs to be changed for it to work on your computer (duh)
+const ABS_ROOT_IMG_FOLDER = 'C:/Users/VilleL/Desktop/ClothesPin/ml-train/fetch-images/images/'; 
+
+// used when dl'ding the images... it hides the 'C:' for some reason. -.- again, change this for yourself
+const ROOT_DEST_FOLDER = '/Users/VilleL/Desktop/ClothesPin/ml-train/fetch-images/images/'; 
+
+// ***************************** CREATE IMAGE DIRECTORIES IF THEY DON'T EXIST ******************************************************************************************************
+
+// this is only needed because git doesn't want to commit empty folders -.-
+createImageFolders();
 
 // ***************************** CREATE SCHEMAS + DB *******************************************************************************************************************************
 
@@ -35,55 +45,78 @@ const ImageObjSchema = {
   name: 'ImageObj',
   properties: {
     url:  'string',
-    price: 'float'
+		price: 'float'
   }
 };
 
 const CategorySchema = {
   name: 'Category',
   properties: {
-    name:  'string',
-	images: 'ImageObj[]'
+		name:  'string',
+		images: 'ImageObj[]'
   }
 };
 
 const imageRealm = new Realm({
-  path: 'image.realm',
+  path: 'imagez.realm',
   schema: [ImageObjSchema, CategorySchema]
 });
 
 // *************************** DL THE IMAGES & SAVE THEM TO THE DB ******************************************************************************************************************
 
-// there's some complications (stuff becomes undefined) if we try to dl more than 400 items at once, despite the 'await' keyword. 
-// it's not too big of a deal if we can figure out how to add items to existing categories.
-const numOfItemsToDl = 400; 
+const searches = [ ['paita', 'shirts'], ['takki', 'coats'], ['housut', 'pants'], ['kengät', 'shoes'], ['hattu', 'hats'] ];
 
-// just uncomment the one that you want to do... I tried mucking about with Promise.all to do 
-// all of these at once, but it seems I just can't get it right. You *could* try to run these 
-// one after the other, but the database might have some sync issues because of it... I'd do these
-// one by one, just to be safe.
+// there's some complications (stuff becomes undefined) if we try to dl more than 400 items at once, despite the 'await' keyword,
+// so it's best to do the downloads in batches
+const numOfItemsToDl = 400;
 
-doFetch('paita', 'shirts', numOfItemsToDl);
-// doFetch('takki', 'coats', numOfItemsToDl);
-// doFetch('housut', 'pants', numOfItemsToDl);
-// doFetch('kengät', 'shoes', numOfItemsToDl);
-// doFetch('hattu', 'hats', numOfItemsToDl);
+doFetch(searches, numOfItemsToDl);
 
 // it's needed as a wrapper function in order to use the 'await' keyword
-async function doFetch(term, categoryName, numOfImages) {
+async function doFetch(searchArray, numOfImages) {
+
+	const promises = [];
+
+	for (let i = 0; i < searchArray.length; i++) {
 	
-	const images = await getItems(term, categoryName, numOfImages);
+		// to get four pages of results for each search... inelegant, but ehh, it works
+		// NOTE: if you want pages 5++, you'll have to manually update these calls for now.
+		// if you don't do that, you'll get the same images all over again and they'll
+		// overwrite the old ones!
+		promises.push(getItems(searchArray[i][0], searchArray[i][1], numOfImages, 1));
+		promises.push(getItems(searchArray[i][0], searchArray[i][1], numOfImages, 2));
+		promises.push(getItems(searchArray[i][0], searchArray[i][1], numOfImages, 3));
+		promises.push(getItems(searchArray[i][0], searchArray[i][1], numOfImages, 4));
+	}
+
+	const resultsArray = await Promise.all(promises);
+
+	let j = 0;
+
+	for (let i = 0; i < resultsArray.length; i++) {
+
+		// searchArray contains 5 items, so the index to obtain the correct categoryName must loop around
+		if (j > 4) {
+			j = 0;
+		}
+
+		let categoryName = searchArray[j][1];
+		j++;
 	
-	saveToRealmDb(images, categoryName);
-}
+		// pretty damn 'loopy' solution, but we need the relevant categoryName for this call, and it's not returned with the results.
+		// another option would be to add the categoryName to the result (either return an object with that field and an array field 
+		// instead of a 'plain' array -- OR add the category field to every ImageObj just for this one use), but I'm not sure if either is really a better solution.
+		saveToRealmDb(resultsArray[i], categoryName);
+	}
+} // end doFetch()
 
 // it both downloads the images and returns the objects that contain the local urls, 
 // which breaks separation of concerns... meh, it was highly convenient and we only need to do this once.
-function getItems(term, category, numOfImages) {
+function getItems(term, category, numOfImages, resultPageNumber) {
 	
 	const items = [];
 	
-	return fetch(BASE_API_URL + `items?words=${term}&category=561&sellstyle=buy-now&limit=500`) // category 561 = 'clothing'; it can be hard-coded as it never changes
+	return fetch(BASE_API_URL + `items?words=${term}&category=561&sellstyle=buy-now&limit=${numOfImages}&page=${resultPageNumber}`) // category 561 = 'clothing'; it can be hard-coded as it never changes
     .then(response => {
 		if (response.ok) {
 			return response.json();
@@ -91,18 +124,19 @@ function getItems(term, category, numOfImages) {
     })
     .then(myJson => {
 		
-		const someItems = [];
+		const validItems = [];
 		
 		for (let i = 0; i < numOfImages; i++) {
-			
-			someItems.push(myJson.items[i]);
+
+			 // it seems there are no images for some sales entries, and some entries are even invalid
+			if (myJson.items[i] !== undefined && myJson.items[i].images[0] !== undefined) {
+
+				validItems.push(myJson.items[i]);
+			}
 		}
 			
-		someItems.map(item => {
-			
-			// it seems the 'images' array can be undefined for some items
-			if (item.images[0]) {
-				
+		validItems.map(item => {
+							
 				const dlUrl = item.images[0].links.thumbnail; // 'thumbnail' = 140 x 140 pixel image; 'medium' varies, but is usually something like 400-600 x 400-600 px
 				
 				const options = {
@@ -112,9 +146,9 @@ function getItems(term, category, numOfImages) {
 				
 				download.image(options)
 			    .then(({ filename, image }) => {
-					console.log('File saved to', filename)
+						// console.log('File saved to', filename)
 			    }).catch((err) => {
-					console.log(err.message);
+						console.log(err.message);
 			    });
 				
 				// changing the image url to point to the locally saved image. 
@@ -130,7 +164,6 @@ function getItems(term, category, numOfImages) {
 				const img = new ImageObj(localUrl, price);
 				
 				items.push(img);
-			} // end if
 		}); // end map()
 		
 		return items;
@@ -139,11 +172,38 @@ function getItems(term, category, numOfImages) {
 } // end getItems()
 
 function saveToRealmDb(arrayOfImageObjs, categoryName) {
-	
-	const cat = new Category(categoryName, arrayOfImageObjs); // for now, it creates a new category each time, preventing us from adding images to existing ones... TODO: fix asap!
 
 	imageRealm.write(() => {
-		imageRealm.create('Category', {name: cat.name, images: cat.images});
-		console.log("wrote category to Realm!");
-	});
+
+		// apparently backticks are a no-no here... looks weird but it works, so, whatever
+		let existingCat = imageRealm.objects('Category').filtered('name = ' + '"' + categoryName + '"')[0];
+
+		if (existingCat === null || existingCat === undefined) {
+		
+			const newCat = new Category(categoryName, arrayOfImageObjs);
+
+			imageRealm.create('Category', {name: newCat.name, images: newCat.images});
+			console.log("wrote new category to Realm!");
+
+		} else {
+
+				arrayOfImageObjs.map(imageObj => {
+
+					existingCat.images.push(imageObj);			
+					console.log("wrote objects to existing Realm category!");
+				});
+		}	
+	}); // end write()
 } // end saveToRealmDb()
+
+function createImageFolders() {
+
+	const imgFolderPaths = ['./images/shirts', './images/coats', './images/shoes', './images/pants', './images/hats'];
+
+	imgFolderPaths.map(path => {
+	
+		fse.ensureDir(path, err => {
+			console.log(err) // => prints 'null' if it succeeds
+		})
+	});
+} // end createImageFolders()
